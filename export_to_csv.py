4d795ca9e0c3f8b19ebe95deb000d7984cdfe41c
Konstantin Zavgorodniy <zavkostya@gmail.com>
2022-09-27 00:25:51 -0400

    completed conversion to flat table (csv)

--- export_to_csv.py
+++ export_to_csv.py
@@ -6,6 +6,17 @@ import argparse
 import pandas
 
 
+YEAR_COLUMNS = ['keeper','price','cap%','avg_proj','avg','proj_total','total']
+YEAR_VALUES =  [
+    'draft_keeper',
+    'draft_price',
+    'draft_cap_percentage',
+    '',
+    '',
+    '',
+    ''
+    ]
+
 def get_args() -> argparse.Namespace:
     """
     Get command line arguments
@@ -39,7 +50,7 @@ def setup_df(_full_data) -> pandas.DataFrame:
         df = pandas.concat([df, tmp_df])
 
     # expand dataframe with columns for players. all cells have NaN
-    header_values = [reversed(_full_data['years']), ['keeper','price','cap%','avg_proj','avg','proj_total','total']]
+    header_values = [reversed(_full_data['years']), YEAR_COLUMNS]
     header = pandas.MultiIndex.from_product(header_values)
     years_df = pandas.DataFrame(columns=header)
 
@@ -49,27 +60,27 @@ def setup_df(_full_data) -> pandas.DataFrame:
     return df
 
 
-def get_players_data_as_df(_players_data, _years) -> pandas.DataFrame:
+# def get_players_data_as_df(_players_data, _years) -> pandas.DataFrame:
 
-    # print(player_df)
-    # _indx_df = pandas.DataFrame({'id': [_players_data['id']], 'name':[_players_data['name']]}).set_index('id')
-    # print(_indx_df)
+#     # print(player_df)
+#     # _indx_df = pandas.DataFrame({'id': [_players_data['id']], 'name':[_players_data['name']]}).set_index('id')
+#     # print(_indx_df)
 
-    # _data_schema_df = dict()
-    # _data_schema_df[_year] = pandas.DataFrame(
-    #     columns=['keeper','price','cap%','avg proj','avg','proj total','total'],
-    #     data=[[False, None, None, None, None, None, None]]
-    # )
-    # print(_data_schema_df)
-    # df = pandas.concat([_indx_df, _data_schema_df])
-    # print(df)
-    # if not _players_data[_year]['stats']:
-    #     # handler for stats
-    #     pass
-    # else:
-    #     # handler for no stats
-    #     pass
-    return player_df
+#     # _data_schema_df = dict()
+#     # _data_schema_df[_year] = pandas.DataFrame(
+#     #     columns=['keeper','price','cap%','avg proj','avg','proj total','total'],
+#     #     data=[[False, None, None, None, None, None, None]]
+#     # )
+#     # print(_data_schema_df)
+#     # df = pandas.concat([_indx_df, _data_schema_df])
+#     # print(df)
+#     # if not _players_data[_year]['stats']:
+#     #     # handler for stats
+#     #     pass
+#     # else:
+#     #     # handler for no stats
+#     #     pass
+#     return player_df
     
 
 def save_to_csv(_df, _league_id):
@@ -91,13 +102,43 @@ def main(_league_id) -> pandas.DataFrame:
 
     dict_data = load_json(_league_id)
     df = setup_df(dict_data)
-
+    idx = pandas.IndexSlice
     
-    # for pid in dict_data['players']:
-    #     player_data = get_players_data_as_df(dict_data['players'][pid], dict_data['years'])
-    #     df = pandas.concat([df, player_data])
-    #     print(df)
-    #     pass
+    for pid in dict_data['players']:
+        player_dict = dict_data['players'][pid]
+        for year in dict_data['years']:
+            try:
+                if player_dict[str(year)]['stats']:
+                    yd = dict(player_dict[str(year)])
+                    values = list()
+
+                    values.append(yd['draft_keeper']) if 'draft_keeper' in yd.keys() else values.append('')
+                    values.append(yd['draft_price']) if 'draft_price' in yd.keys() else values.append('')
+                    values.append(yd['draft_cap_percentage']) if 'draft_cap_percentage' in yd.keys() else values.append('')
+
+                    values.append(yd['stats']['Projected {}'.format(year)]['total']['f_avg']) if 'Projected {}'.format(year) in yd['stats'].keys() else values.append('')
+                    values.append(yd['stats']['Total {}'.format(year)]['total']['f_avg']) if 'Total {}'.format(year) in yd['stats'].keys() else values.append('')
+                    values.append(yd['stats']['Projected {}'.format(year)]['total']['f_total']) if 'Projected {}'.format(year) in yd['stats'].keys() else values.append('')
+                    values.append(yd['stats']['Total {}'.format(year)]['total']['f_total']) if 'Total {}'.format(year) in yd['stats'].keys() else values.append('')
+
+                    df.loc[int(pid), idx[year, YEAR_COLUMNS]] = values
+                    # print(df.loc[int(pid)])
+                    # print(df)
+                    # print()
+                else:
+                    # empty stats data for the year
+                    continue
+            except KeyError as ex:
+                # print(ex)
+                # no stats data for the year
+                continue
+        # df.loc[]
+
+        # player_data = get_players_data_as_df(dict_data['players'][pid], dict_data['years'])
+        # df = pandas.concat([df, player_data])
+        # print(df)
+        # pass
+
 
 
     print(df)
